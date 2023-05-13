6022337a0fd86618d7643f52621c51017e7037da
notrealtrick <97556531+notrealtrick@users.noreply.github.com>
2023-05-13 20:39:22 +0300

    Proje dosyası ekle.

---
+++ KSS/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace KSS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.giris = new System.Windows.Forms.Button();
+            this.uyeol = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // giris
+            // 
+            this.giris.Location = new System.Drawing.Point(207, 269);
+            this.giris.Name = "giris";
+            this.giris.Size = new System.Drawing.Size(75, 23);
+            this.giris.TabIndex = 0;
+            this.giris.Text = "Giriş yap";
+            this.giris.UseVisualStyleBackColor = true;
+            this.giris.Click += new System.EventHandler(this.giris_Click);
+            // 
+            // uyeol
+            // 
+            this.uyeol.Location = new System.Drawing.Point(446, 269);
+            this.uyeol.Name = "uyeol";
+            this.uyeol.Size = new System.Drawing.Size(75, 23);
+            this.uyeol.TabIndex = 1;
+            this.uyeol.Text = "Üye Ol";
+            this.uyeol.UseVisualStyleBackColor = true;
+            this.uyeol.Click += new System.EventHandler(this.uyeol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uyeol);
+            this.Controls.Add(this.giris);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button giris;
+        private System.Windows.Forms.Button uyeol;
+    }
+}
+
