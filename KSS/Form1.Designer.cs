a7d5da88618d2d4c75b9fb135760e78286a0314d
notrealtrick <97556531+notrealtrick@users.noreply.github.com>
2023-05-15 22:12:20 +0300

    dizayn

--- KSS/Form1.Designer.cs
+++ KSS/Form1.Designer.cs
@@ -102,6 +102,7 @@
             this.Controls.Add(this.giris);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- KSS/Form1.cs
+++ KSS/Form1.cs
@@ -35,5 +35,10 @@ namespace KSS
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
